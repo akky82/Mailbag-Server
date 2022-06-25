1966feef9430317e6b213f197e7b236e3d589f75
Damon <damonlhill82@gmail.com>
2022-06-25 23:46:58 +1000

    first commit, defined interface

--- src/ServerInfo.ts
+++ src/ServerInfo.ts
@@ -1,3 +1,29 @@
 /* This will be a configuration file that provides details
 about the IMAP and SMTP server(s) the server will connect to and
 where that information will be stored. */
+
+const path = require("path");
+const fs = require("fs");
+
+export interface IServerInfo {
+    smtp : {
+        host: string,
+        port: number,
+        auth: {
+            user: string,
+            pass: string
+        }
+    },
+    imap : {
+        host: string,
+        port: number,
+        auth: {
+            user: string,
+            pass: string
+        }
+    }
+}
+
+export let serverInfo: IServerInfo;
+const rawInfo: string = fs.readFileSync(path.join(__dirname, "../serverInfo.json"));
+serverInfo = JSON.parse(rawInfo);
