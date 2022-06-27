7f381083df95c428847d97aa751c8b8df2faa17d
Damon <damonlhill82@gmail.com>
2022-06-27 13:07:19 +1000

    Created smtp worker for sending messages

--- src/SMTP.ts
+++ src/SMTP.ts
@@ -1,2 +1,35 @@
 /* Code that talks to an SMTP server to send messages. Like
 IMAP, this will have a Worker class. */
+
+import Mail from "nodemailer/lib/mailer";
+import * as nodemailer from "nodemailer";
+import { SendMailOptions, SentMessageInfo } from "nodemailer";
+import { IServerInfo } from "./ServerInfo";
+
+// const nodemailer = require("nodemailer");
+
+export class Worker {
+    // Server information
+    private static serverInfo: IServerInfo;
+
+    // Constructor
+    constructor(inServerInfo: IServerInfo) {
+        Worker.serverInfo = inServerInfo;
+    }
+
+
+    public sendMessage(inOptions: SendMailOptions): Promise<string> {
+        return new Promise((inResolve, inReject) => {
+            const transport: Mail = nodemailer.createTransport(Worker.serverInfo.smtp);
+            transport.sendMail(
+                inOptions,
+                (inError: Error | null, inInfo: SentMessageInfo) => {
+                    if (inError) {
+                        inReject(inError);
+                    } else {
+                        inResolve();
+                    }
+                });
+        });
+    }
+}
